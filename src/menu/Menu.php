28296805d04f71ef1b8c3870b9102e631e1b7b3d
Garung <canmotcaiten1993@gmail.com>
2017-09-03 08:31:46 +0700

    update tag 1.0, add function renderView for main menu class

--- src/menu/Menu.php
+++ src/menu/Menu.php
@@ -8,10 +8,15 @@ class Menu
 {
 	protected $name_menu = 'Menu1';
 	protected $params = [];
+	public $menu;
 	function __construct($name_menu, $params)
 	{
 		$this->name_menu = $name_menu;
 		$this->params = $params;
-		$menu = new $this->name_menu($params);
+		$this->menu = new $this->name_menu($params);
+	}
+
+	function renderView() {
+		return $this->menu->renderView();
 	}
 }
